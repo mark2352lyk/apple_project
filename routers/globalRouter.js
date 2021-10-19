794708b12318b0244ade81cd950eac9c72318bdc
이유겸 <mark2352lyk@gmail.com>
2021-10-19 19:56:09 +0900

    mac,id,iphone,login,review,watch,ipad.pug

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -6,4 +6,28 @@ router.get("/", (req, res, next) => {
     res.render("screens/main");
 });
 
+router.get("/iphone", (req,res,next) => {
+    res.render("screens/iphone");
+});
+
+router.get("/ipad", (req,res,next) => {
+    res.render("screens/ipad");
+});
+
+router.get("/mac", (req,res,next) => {
+    res.render("screens/mac");
+});
+
+router.get("/watch", (req,res,next) => {
+    res.render("screens/watch");
+});
+
+router.get("/review", (req,res,next) => {
+    res.render("screens/review");
+});
+
+router.get("/login", (req,res,next) => {
+    res.render("screens/login");
+});
+
 module.exports = router;
